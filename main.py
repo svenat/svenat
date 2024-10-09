d245219a54b7e6ffc398ffcaad6fe13b2cf0b7c1
sventjallingii <tjallingii.sven@gmail.com>
2024-10-09 17:13:24 +0200

    hello world

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ pygame.init()
 scWidth = 960
 scHeight = 540
 mainSurface = pygame.display.set_mode((scWidth, scHeight))
+mainBackground = pygame.image.load('IMAGES/Black Background.jpg')
 
 pygame.display.set_caption('Memory Game')
 fsPS = pygame.time.Clock()
 
 startFont = pygame.font.Font('FONTS/SaucerFont.ttf', 50)
 startText = startFont.render('Start The Game', False, 'Red')
+startMovementText = 50
+startMovementPlanet = 150
 
 creditSurface = pygame.Surface((scWidth, 30))
 creditSurface.fill('white')
@@ -23,20 +26,36 @@ planetStart = pygame.image.load('IMAGES/STart the Game.png')
 
 
 
-
 while True:
 
 
-
+#Main run Screen
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			exit()
 
-
-	mainSurface.blit(planetStart, (0, 150))
+#Starting Screen Background
+	mainSurface.blit(mainBackground, (0, 0))
 	mainSurface.blit(creditSurface, (0, 510))
-	mainSurface.blit(startText, (360, 50))
+#Starting Screen Toppings
+
+#key buttons
+	key = pygame.key.get_pressed()
+	if key[pygame.K_SPACE] == True:
+		while startMovementText > (-50):
+			startMovementText -= 50
+	if key[pygame.K_SPACE] == True:
+		while startMovementPlanet < (540):
+			startMovementPlanet += 50
+
+
+	mainSurface.blit(startText, (360, startMovementText))
+
+	mainSurface.blit(planetStart, (0, startMovementPlanet))
+
+
+
 
 	pygame.display.update()
 	clock.tick(60)
